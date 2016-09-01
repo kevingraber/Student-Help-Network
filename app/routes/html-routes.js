0cf75638531d425c97257278b7fde4b18ad1756a
kg <kjg310@gmail.com>
2016-09-01 10:16:54 -0400

    added new / path

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -32,6 +32,11 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname+'/../public/index.html'));
 	});
 
+	app.get('/',
+	function(req, res){
+		res.sendFile(path.join(__dirname+'/../public/index.html'));
+	});
+
 	app.get('/admin',
 	function(req, res){
 		res.sendFile(path.join(__dirname+'/../public/admin.html'));
